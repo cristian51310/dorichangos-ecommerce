b7553c22e5a17e38316c40df2b32aac472bf408f
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2024-01-08 10:10:43 -0600

    fix landing page mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,13 @@ import Title from "@/components/landing/title";
 
 export default function LandingPage() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center mb-24">
+    <div className="min-h-screen relative flex flex-col items-center">
       <Title />
+
+      <h3 className="text-xl lg:text-2xl xl:text-3xl text-pink-700 max-w-2xl md:px-0 px-8">
+        Inserte una frase aqui que haga que la gente quiera comprar, puede ser algun eslogan o algo asi
+      </h3>
+
       <BannerMuseo />
     </div>
   );
--- components/landing/bannerMuseo.tsx
+++ components/landing/bannerMuseo.tsx
@@ -3,15 +3,13 @@ import Link from "next/link";
 
 export default function BannerMuseo() {
   return (
-    <div className="bg-orange-200 fixed bottom-0 left-24 right-24 h-16 rounded-t-full cursor-pointer">
+    <div className="bg-orange-200 absolute bottom-0 left-0 right-0 h-16 rounded-t-3xl cursor-pointer">
       <div className="flex items-center justify-center h-full">
-        <span className="flex items-center gap-4 hover:gap-6 transition-all duration-300">
-
-          <Link href="/home" className="text-center font-bold text-xl">
+        <span className="flex items-center gap-2 md:gap-4 hover:gap-6 transition-all duration-300">
+          <Link href="/home" className="text-center font-bold text-base md:text-xl">
             Tunel de los remedios, reserva tu visita aqui
           </Link>
-
-          <ArrowRight className="w-6 h-6" />
+          <ArrowRight className="md:w-6 w-5" />
         </span>
       </div>
     </div>
